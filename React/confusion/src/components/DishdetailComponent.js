5a79123e3df1319e951b67a4dd60f62e70c7ff2e
instaboy007 <ssprashanth2002@gmail.com>
2022-01-28 13:57:28 +0530

    functional component are used in place of class components where the components purely depend on props and not on states

--- React/confusion/src/components/DishdetailComponent.js
+++ React/confusion/src/components/DishdetailComponent.js
@@ -1,78 +1,74 @@
-import React, {Component} from 'react';
+import React from 'react';
 import { Card, CardImg, CardText, CardBody,CardTitle } from 'reactstrap';
 
-class DishDetail extends Component{
-    constructor(props){
-        super(props);
-        this.state={};
-    }
-
-    renderDish(dish) {
-        if (dish != null){
-            return(
-                <Card key={dish.id}>
-                    <CardImg top src={dish.image} alt={dish.name} />
-                    <CardBody>
-                      <CardTitle>{dish.name}</CardTitle>
-                      <CardText>{dish.description}</CardText>
-                    </CardBody>
-                </Card>
-            );
-        }
-        else{
-            return(
-                <div></div>
-            );
-        }
-    }
-
-    renderComments(dish){
-        if(dish==null){
-            return(
-                <div></div>
-            )
-        }
-        else{
-            const dishComments=dish.comments.map((comment)=>{
-                console.log(comment);
-                var date = new Date(comment.date);
-                var day = date.getDate();
-                const monthNames = ["January", "February", "March", "April", "May", "June","July", "August", "September", "October", "November", "December"];
-                var month = date.getMonth();
-                var year = date.getFullYear();
-                return(
-                    <li>
-                        <p>{comment.comment}</p>
-                        <p>-- {comment.author}, {monthNames[month].slice(0,3)} {day}, {year}</p>
-                    </li>
-                )     
-            });
-            return(
-                <div>
-                    <header>
-                        <strong><h4>Comments</h4></strong>
-                    </header>
-                    
-                    <ul className="list-unstyled">{dishComments}</ul>
-                </div>
-                
-            )
-        }
-    }
-
-    render(){
-        const dish=this.props.selectedDish;
+function RenderDish({dish}){
+    if (dish != null){
         return(
-            <div className='row'>
-                <div className='col-12 col-md-5 m-1'>
-                    {this.renderDish(dish)}
-                </div>
-                <div className='col-12 col-md-5 m-1'>
-                    {this.renderComments(dish)}
-                </div>
-            </div>
+            <Card key={dish.id}>
+                <CardImg top src={dish.image} alt={dish.name} />
+                <CardBody>
+                    <CardTitle>{dish.name}</CardTitle>
+                    <CardText>{dish.description}</CardText>
+                </CardBody>
+            </Card>
+        );
+    }
+    else{
+        return(
+            <div></div>
+        );
+    }
+}
+
+function RenderComments({dish}){
+    if(dish==null){
+        return(
+            <div></div>
         )
     }
+    else{
+        const dishComments=dish.comments.map((comment)=>{
+            var date = new Date(comment.date);
+            var day = date.getDate();
+            const monthNames = ["January", "February", "March", "April", "May", "June","July", "August", "September", "October", "November", "December"];
+            var month = date.getMonth();
+            var year = date.getFullYear();
+            return(
+                <li key={comment.id}>
+                    <p>{comment.comment}</p>
+                    <p>-- {comment.author}, {monthNames[month].slice(0,3)} {day}, {year}</p>
+                </li>
+            )     
+        });
+        return(
+            <div key={dish.id}>
+                <header>
+                    <strong><h4>Comments</h4></strong>
+                </header>
+                <ul className="list-unstyled">{dishComments}</ul>
+            </div>
+            
+        )
+    }
+}
+
+
+
+const DishDetail=(props)=>{
+    const dish=props.dish;
+    return(
+        <div className='container'>
+            <div className='row'>
+                <div className='col-12 col-md-5 m-1'>
+                    <RenderDish dish={dish} />
+                </div>
+                <div className='col-12 col-md-5 m-1'>
+                    <RenderComments dish={dish} />
+                </div>
+            </div>
+        </div>
+        
+    )
 }
 
 export default DishDetail;
