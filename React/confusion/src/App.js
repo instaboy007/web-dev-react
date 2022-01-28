4af6763ae7ad4685db8b8215c7c455df75e7ad6a
instaboy007 <ssprashanth2002@gmail.com>
2022-01-28 21:12:31 +0530

    react router

--- React/confusion/src/App.js
+++ React/confusion/src/App.js
@@ -1,5 +1,6 @@
 import React,{Component} from 'react';
 import Main from './components/MainComponent';
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 
 class App extends Component{
@@ -10,9 +11,11 @@ class App extends Component{
   }
   render(){
     return (
-      <div className='App'>
-        <Main/>
-      </div>
+      <BrowserRouter>
+        <div className='App'>
+          <Main/>
+        </div>
+      </BrowserRouter> 
     );
   }
 };
--- React/confusion/src/components/MainComponent.js
+++ React/confusion/src/components/MainComponent.js
@@ -1,8 +1,12 @@
 import React,{Component} from 'react';
-import {Navbar,NavbarBrand} from 'reactstrap';
 import { DISHES } from '../shared/dishes';
+import Header from './HeaderComponent';
+import Footer from './FooterComponent';
 import Menu from './MenuComponent'
 import DishDetail from './DishdetailComponent';
+import Home from './HomeComponent';
+import { Switch, Route, Redirect } from 'react-router-dom';
+
 
 class Main extends Component{
     constructor(props) {
@@ -18,15 +22,21 @@ class Main extends Component{
     }
 
   render(){
+    const HomePage = () => {
+      return(
+          <Home 
+          />
+      );
+    }
     return (
       <div>
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-          </div>
-        </Navbar>
-        <Menu dishes={this.state.dishes} onClick={(dishID) => this.onDishSelect(dishID)}/>
-        <DishDetail dish={this.state.dishes.filter((dish)=> dish.id === this.state.selectedDish)[0]} />
+        <Header />
+        <Switch>
+          <Route path='/home' component={HomePage} />
+          <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+          <Redirect to="/home" />
+        </Switch>
+        <Footer />
       </div>
     );
   }
