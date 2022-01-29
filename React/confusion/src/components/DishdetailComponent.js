e0a62494524e4d2273afbcbedb80c25689293093
instaboy007 <ssprashanth2002@gmail.com>
2022-01-29 16:51:27 +0530

    assignment2-answer

--- React/confusion/src/components/DishdetailComponent.js
+++ React/confusion/src/components/DishdetailComponent.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Card, CardImg, CardText, CardBody,CardTitle } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody,CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 function RenderDish({dish}){
     if (dish != null){
@@ -27,7 +28,7 @@ function RenderComments({dish}){
         )
     }
     else{
-        const dishComments=dish.comments.map((comment)=>{
+        const dishComments=dish.map((comment)=>{
             var date = new Date(comment.date);
             var day = date.getDate();
             const monthNames = ["January", "February", "March", "April", "May", "June","July", "August", "September", "October", "November", "December"];
@@ -55,15 +56,24 @@ function RenderComments({dish}){
 
 
 const DishDetail=(props)=>{
-    const dish=props.dish;
     return(
         <div className='container'>
+            <div className="row">
+                <Breadcrumb>
+                    <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                    <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                </Breadcrumb>
+                <div className="col-12">
+                    <h3>{props.dish.name}</h3>
+                    <hr />
+                </div>                
+            </div>
             <div className='row'>
                 <div className='col-12 col-md-5 m-1'>
-                    <RenderDish dish={dish} />
+                    <RenderDish dish={props.dish} />
                 </div>
                 <div className='col-12 col-md-5 m-1'>
-                    <RenderComments dish={dish} />
+                    <RenderComments dish={props.comments} />
                 </div>
             </div>
         </div>
--- React/confusion/src/components/MainComponent.js
+++ React/confusion/src/components/MainComponent.js
@@ -4,9 +4,10 @@ import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Menu from './MenuComponent'
 import DishDetail from './DishdetailComponent';
+import About from './AboutComponent';
 import Contact from './ContactComponent';
 import Home from './HomeComponent';
-import { Switch, Route, Redirect } from 'react-router-dom';
+import {Switch, Route, Redirect } from 'react-router-dom';
 import { COMMENTS } from '../shared/comments';
 import { PROMOTIONS } from '../shared/promotions';
 import { LEADERS } from '../shared/leaders';
@@ -38,13 +39,28 @@ class Main extends Component{
           />
       );
     }
+    const DishWithId = ({match}) => {
+      return(
+          <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
+            comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
+      );
+    };
+
+    const AboutPage=()=>{
+      return(
+        <About leaders={this.state.leaders}/>
+      )
+    }
+
     return (
       <div>
         <Header />
         <Switch>
           <Route path='/home' component={HomePage} />
           <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
-          <Route exact path='/contactus' component={Contact} />} />
+          <Route path='/menu/:dishId' component={DishWithId} />
+          <Route exact path='/aboutus' component={AboutPage}/>
+          <Route exact path='/contactus' component={Contact} />
           <Redirect to="/home" />
         </Switch>
         <Footer />
